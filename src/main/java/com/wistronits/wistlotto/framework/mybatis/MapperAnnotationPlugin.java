ed4a847063ab02bca09dab87d2a438ba77e519c3
bullsgl <bullsgl@gmail.com>
2018-10-08 15:18:45 +0800

    ignore

--- src/main/java/com/wistronits/wistlotto/framework/mybatis/MapperAnnotationPlugin.java
+++ src/main/java/com/wistronits/wistlotto/framework/mybatis/MapperAnnotationPlugin.java
@@ -1,5 +1,23 @@
 package com.wistronits.wistlotto.framework.mybatis;
 
-public class MapperAnnotationPlugin {
+import java.util.List;
 
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+public class MapperAnnotationPlugin extends PluginAdapter {
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        interfaze.addImportedType(new FullyQualifiedJavaType("org.apache.ibatis.annotations.Mapper")); //$NON-NLS-1$
+        interfaze.addAnnotation("@Mapper"); //$NON-NLS-1$
+        return true;
+    }
 }
--- src/main/java/com/wistronits/wistlotto/framework/mybatis/RenameMapperClassPlugin.java
+++ src/main/java/com/wistronits/wistlotto/framework/mybatis/RenameMapperClassPlugin.java
@@ -1,5 +1,20 @@
 package com.wistronits.wistlotto.framework.mybatis;
 
-public class RenameMapperClassPlugin {
+import java.util.List;
 
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+public class RenameMapperClassPlugin extends PluginAdapter {
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public void initialized(IntrospectedTable table) {
+        super.initialized(table);
+        String name = table.getMyBatis3JavaMapperType();
+        table.setMyBatis3JavaMapperType(name.replaceAll("Mapper$", "Repository")); //$NON-NLS-1$
+    }
 }
