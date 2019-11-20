af21f0a9e610fa18a18679868d549dd524e83ed2
bullsgl <bullsgl@gmail.com>
2019-11-20 18:25:09 +0800

    Layout Modify

--- src/main/resources/db/migration/V0_0_1__create.sql
+++ src/main/resources/db/migration/V0_0_1__create.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS t_emp_info (
     emp_id  TEXT NOT NULL PRIMARY KEY ASC,                      -- 员工ID（主键，不能重复）
     emp_name TEXT NOT NULL,                                     -- 员工姓名
-    emp_sex TEXT NOT NULL,                                      -- 性别
+    emp_sex TEXT NOT NULL,                                      -- 员工性别
     dept_id TEXT NOT NULL,                                      -- 员工所在部门ID
     emp_rate INTEGER,                                           -- 中奖权值（越大概率越高）
     prize_flag TEXT NOT NULL                                    -- 中奖标识（0为未中奖，1为已中奖，9为已放弃）
@@ -14,11 +14,11 @@ CREATE TABLE IF NOT EXISTS t_prize_info (
     prize_name TEXT NOT NULL,                                   -- 奖项名称
     prize_desc TEXT NOT NULL,                                   -- 奖项描述
     prize_order INTEGER NOT NULL,                               -- 抽选顺序
-    prize_number INTEGER NOT NULL,                              -- 奖品数量
+    prize_number INTEGER NOT NULL,                              -- 奖项数量
     prize_winner INTEGER NOT NULL,                              -- 中奖人数
     dept_id TEXT,                                               -- 限定部门ID（为空时，不限定）
     prize_multi TEXT NOT NULL,                                  -- 是否允许重复中奖（0为不允许，1为允许）
-    prize_status TEXT NOT NULL                                  -- 奖项状态
+    prize_status TEXT NOT NULL                                  -- 抽选状态
 );
 
 -- 中奖表
