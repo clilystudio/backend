26b60c264ee9cdb929bcf8ee75772c790cf85a40
ShengGL <bullsgl@gmail.com>
2020-01-13 22:03:13 +0800

    External Resource

--- src/main/java/com/wistronits/wistlotto/config/MvcConfig.java
+++ src/main/java/com/wistronits/wistlotto/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.wistronits.wistlotto.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/ext/**").addResourceLocations("file:///C:/wistlotto/ext/");
+	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
